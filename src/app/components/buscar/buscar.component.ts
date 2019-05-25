85085260b6ea912c2555d92cfd9dd685368e19a9
hamlet10 <hpsanchez@live.com>
2019-05-25 10:04:05 -0400

    No furula, se chingo

--- src/app/components/buscar/buscar.component.ts
+++ src/app/components/buscar/buscar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { PeliculasService } from "../../providers/peliculas.service";
 
 @Component({
-  selector: 'app-buscar',
-  templateUrl: './buscar.component.html',
+  selector: "app-buscar",
+  templateUrl: "./buscar.component.html",
   styles: []
 })
 export class BuscarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  _ps: PeliculasService;
+  ngOnInit() {}
 
-  ngOnInit() {
+  //No furula
+  buscar_pelicula(texto: string) {
+    console.log(texto);
+    this._ps.buscarPelicula(texto).subscribe((data: any) => console.log(data));
   }
-
 }
